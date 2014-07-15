82fc9a41e6d2290ef7d67ac02032bc359875526b
Tom Faris <ta.faris@gmail.com>
2014-07-15 00:47:59 -0400

    Fix call to Popen for linux.

--- django_project/views.py
+++ django_project/views.py
@@ -7,7 +7,7 @@ __author__ = 'tfaris'
 
 def add_view(request):
     addend = request.GET.get('a', 0)
-    p = subprocess.Popen('mono lib/ConsoleApplication1.exe ' + str(addend), stdout=subprocess.PIPE)
+    p = subprocess.Popen(['mono', 'lib/ConsoleApplication1.exe', str(addend)], stdout=subprocess.PIPE)
     #p = subprocess.Popen('lib/ConsoleApplication1.exe ' + str(addend), stdout=subprocess.PIPE)
     p.wait()
     out = p.stdout.read().strip()
